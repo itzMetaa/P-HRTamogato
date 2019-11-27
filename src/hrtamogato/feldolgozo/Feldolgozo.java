c7d3cf8a5dcfcc6ba3ff1faceeb642b52033d748
TorokNiki <niki.torok@gmail.com>
2019-11-27 15:29:56 +0100

    file beolvas 2.0

--- src/hrtamogato/feldolgozo/Feldolgozo.java
+++ src/hrtamogato/feldolgozo/Feldolgozo.java
@@ -11,13 +11,45 @@ public class Feldolgozo {
             BufferedReader br= new BufferedReader(f);
             String sor= br.readLine();
             int index=0;
+
             for (int i = 0; i < N ; i++) {
+                String fname;
+                String lname;
+                boolean sex;
+                List<String> hobbik;
+                List<String> skils;
+                int promotions
                 while (sor != null) {
                     String[] st = sor.split(":");
-                    if (N==5){
-                        jelentkezok.add(new Jelentkezo());
+                    if (st[0].eqals("First name")){
+                        fname=st[1];
+                    }else if (st[0].eqals("Last name")){
+                        lname=st[1];
+                    }else if (st[0].eqals("Sex")){
+                        if (st[1].eqals("Male")){
+                            sex=true;
+                        }else {
+                            sex=false
+                        }
+                    }else if (st[0].eqals("Hobbies")){
+                        String[] h= st[1].split(",");
+                        for (int j = 0; j < h.length; j++) {
+                            hobbik.add(h[j]);
+                        }
+                    }else if (st[0].eqals("Skills")) {
+                        String[] h = st[1].split(",");
+                        for (int j = 0; j < h.length; j++) {
+                            skils.add(h[j]);
+                        }
+                    }else if (st[0].eqals("Promotions")) {
+                        promotions=Integer.parseInt(st[1]);
                     }
                     sor = br.readLine();
+                }
+                if (N==5){
+                    jelentkezok.add(new Jelentkezo(fname,lname,sex,hobbik,skils));
+                }else if (N==6){
+                    alkalmazottak.add(new Alkalmazott(fname,lname,sex,hobbik,skils,promotions));
                 }
             }
             br.close();
