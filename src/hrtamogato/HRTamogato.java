6f23d252178cc57453fd3000ac6013740f35ef02
TorokNiki <niki.torok@gmail.com>
2019-11-27 15:06:59 +0100

    file beolvas

--- src/hrtamogato/HRTamogato.java
+++ src/hrtamogato/HRTamogato.java
@@ -2,9 +2,11 @@
 package hrtamogato;
 
 public class HRTamogato {
-
+    public static List<Jelentkezo> jelentkezok;
+    public static List<Alkalmazott> alkalmazottak;
+    public static int N;
     public static void main(String[] args) {
-        
+        N=5;
     }
     
 }
--- src/hrtamogato/feldolgozo/Feldolgozo.java
+++ src/hrtamogato/feldolgozo/Feldolgozo.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hrtamogato.feldolgozo;
 
-/**
- *
- * @author Diak
- */
 public class Feldolgozo {
+
+    public void beolvas(String file){
+        jelentkezok= new ArrayList<>();
+        alkalmazottak= new ArrayList<>();
+        try{
+            FileReader f= new FileReader(file);
+            BufferedReader br= new BufferedReader(f);
+            String sor= br.readLine();
+            int index=0;
+            for (int i = 0; i < N ; i++) {
+                while (sor != null) {
+                    String[] st = sor.split(":");
+                    if (N==5){
+                        jelentkezok.add(new Jelentkezo());
+                    }
+                    sor = br.readLine();
+                }
+            }
+            br.close();
+            f.close();
+        }catch (FileNotFoundException e){
+            System.out.println("Nem található a file");
+        }catch (IOException e){
+            System.out.println("Írás/olvasás/jogusulcsági hiba");
+        }
+
+
+    }
     
 }
